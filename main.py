a2c1e16d7872eb3fe45d4128112ed6e23816890a
zzy77 <15226603631@163.com>
2024-01-03 17:38:14 +0800

    基本功能框架

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+from flask import Flask, request
+
+def getPidWithRE(target:str,pattern:str):
+    pass
+    # todo:使用正则匹配获取pid，返回一个数组
+
+def findTaskInfo(searchStr):
+    pass
+    # todo:使用netstat | grep 命令找到与searchStr匹配的结果，返回字符串
+
+def killTask():
+    pass
+    # todo:用pid结束任务
+
+def startTask():
+    pass
+    # todo:启动任务
+
+
 
 if __name__ == '__main__':
     pass
+    # todo:从远程传入参数，控制任务的启停
